73938122c3786d8a30eb63f0a359e8f49f7f1e47
Jyoti Gaidhani <gaidhani.jyoti@gmail.com>
2022-04-24 15:54:31 +0530

    initial commit

---
+++ src/main/java/com/jyoti/SampleJyotiCaterer/SampleJyotiCatererApplication.java
@@ -0,0 +1,13 @@
+package com.jyoti.SampleJyotiCaterer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleJyotiCatererApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleJyotiCatererApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jyoti/SampleJyotiCaterer/SampleJyotiCatererApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyoti.SampleJyotiCaterer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleJyotiCatererApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
